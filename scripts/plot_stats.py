74da8b9af39f9b7d3883cadd65423224e5dcd499
beyzabyr <byzbyr076@gmail.com>
2026-03-15 20:02:55 +0300

    Update plot_stats.py

--- scripts/plot_stats.py
+++ scripts/plot_stats.py
@@ -4,33 +4,32 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def plot_distributions(csv_input, output_plot, output_stats):
-    # CSV dosyasını okuyoruz
+    # Reading CSV file
     df = pd.DataFrame(pd.read_csv(csv_input))
     
-    # 1. Özet İstatistikleri Hesaplama ve Yazdırma (Mean & Median)
+    # 1. (Mean & Median)
     stats_df = df[['GC_Content_Percent', 'Read_Length', 'Mean_Quality']].agg(['mean', 'median']).round(2)
     print("--- Temel Özet İstatistikler ---")
     print(stats_df)
     
-    # İstatistikleri profesyonelce raporlamak için ayrı bir metin dosyasına da kaydedelim
+
     stats_df.to_csv(output_stats)
 
-    # 2. Görselleştirme (Grafik Çizimi)
-    # Seaborn ile profesyonel bir tema ayarlıyoruz
+    # Visualisation (Graphics)
     sns.set_theme(style="whitegrid")
     fig, axes = plt.subplots(1, 3, figsize=(18, 5))
     
-    # A. GC İçeriği Dağılımı (Histogram + Yoğunluk)
+    # A. GC Content Density (Histogram + Density)
     sns.histplot(df['GC_Content_Percent'], bins=30, kde=True, ax=axes[0], color='skyblue')
     axes[0].set_title('GC Content Distribution', fontsize=14)
     axes[0].set_xlabel('GC Content (%)')
     
-    # B. Okuma Uzunluğu Dağılımı
+    # read lenght distribution
     sns.histplot(df['Read_Length'], bins=30, kde=True, ax=axes[1], color='lightgreen')
     axes[1].set_title('Read Length Distribution', fontsize=14)
     axes[1].set_xlabel('Read Length (bp)')
     
-    # C. Ortalama Kalite Skoru Dağılımı
+    # mean quality score distribution
     sns.histplot(df['Mean_Quality'], bins=30, kde=True, ax=axes[2], color='salmon')
     axes[2].set_title('Mean Quality Score Distribution', fontsize=14)
     axes[2].set_xlabel('Phred Quality Score')
